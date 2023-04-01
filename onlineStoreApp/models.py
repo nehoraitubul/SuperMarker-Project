52a58ae75f2ea5e20a5b44ba763ce1fbe97d6813
Nehorai0258 <nehorai0258@gmail.com>
2023-04-02 02:52:38 +0300

    initial commit

---
+++ onlineStoreApp/models.py
@@ -0,0 +1,130 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Product(models.Model):
+
+    class Meta:
+        db_table = 'products'
+        ordering = ['id']
+
+    UNITS_DESCRIPTION = (('ml', 'Milligram'), ('units', 'Unit'), ('g', 'Gram'), ('kg', 'Kilogram'), ('l', 'Liter'))
+
+    name = models.CharField(max_length=256, db_column='name', null=False, blank=False)
+    catalog_number = models.BigIntegerField(db_column='catalog_number', null=False, blank=False)
+    manufacturer_id = models.ForeignKey('Manufacturer', on_delete=models.RESTRICT, db_column='manufacturer_id', null=True, blank=True)
+    units = models.CharField(max_length=128, db_column='units', choices=UNITS_DESCRIPTION, null=False, blank=False)
+    quantity = models.IntegerField(db_column='quantity', null=False, blank=False)
+    category_id = models.ForeignKey('SubCategory', on_delete=models.RESTRICT, db_column='category_id', null=True, blank=True)
+    discount_status = models.BooleanField( db_column='discount_status', null=False, blank=False, default=False)
+    product_status = models.BooleanField( db_column='product_status', null=False, blank=False, default=True)
+    unit_of_measure = models.CharField(max_length=256, db_column='unit_of_measure', null=False, blank=False)
+    unit_of_measure_price = models.IntegerField(db_column='unit_of_measure_price', null=False, blank=False)
+
+
+
+class Price(models.Model):
+
+    class Meta:
+        db_table = 'prices'
+        ordering = ['id']
+
+    product_id = models.ForeignKey('Product', on_delete=models.RESTRICT, db_column='product_id', null=False, blank=False)
+    retailer_id = models.ForeignKey('Retailer', on_delete=models.RESTRICT, db_column='retailer_id', null=False, blank=False)
+    price = models.SmallIntegerField(db_column='price', null=False, blank=False)
+
+
+
+class Retailer(models.Model):
+
+    class Meta:
+        db_table = 'retailers'
+        ordering = ['id']
+
+    name = models.CharField(max_length=256, db_column='name', null=False, blank=False)
+    last_scan = models.BigIntegerField(db_column='last_scan', null=False, blank=False)
+
+
+
+class Manufacturer(models.Model):
+
+    class Meta:
+        db_table = 'manufacturers'
+        ordering = ['id']
+
+    name = models.CharField(max_length=256, db_column='name', null=False, blank=False)
+    country = models.CharField(max_length=256, db_column='country', null=True, blank=True)
+
+
+
+class Category(models.Model):
+
+    class Meta:
+        db_table = 'categories'
+        ordering = ['id']
+
+    name = models.CharField(max_length=256, db_column='name', null=False, blank=False)
+
+
+
+class SubCategory(models.Model):
+
+    class Meta:
+        db_table = 'sub_categories'
+        ordering = ['id']
+
+    name = models.CharField(max_length=256, db_column='name', null=False, blank=False)
+    category_id = models.ForeignKey('Category', on_delete=models.RESTRICT, db_column='category_id', null=True, blank=True)
+
+
+
+class Promo(models.Model):
+
+    class Meta:
+        db_table = 'promos'
+        ordering = ['id']
+
+    type = models.CharField(max_length=256, db_column='type', null=False, blank=False)
+    percent =  models.SmallIntegerField(max_length=128, db_column='percent', null=True, blank=True)
+    fixed = models.SmallIntegerField(max_length=128, db_column='fixed', null=True, blank=True)
+    x = models.SmallIntegerField(max_length=128, db_column='x', null=True, blank=True)
+    y = models.SmallIntegerField(max_length=128, db_column='y', null=True, blank=True)
+    retailer_id = models.ForeignKey('Retailer', on_delete=models.RESTRICT, db_column='retailer_id', null=False, blank=False)
+
+
+
+class PromoProduct(models.Model):
+
+    class Meta:
+        db_table = 'promos_products'
+        ordering = ['id']
+
+    promo_id = models.ForeignKey('Promo', on_delete=models.RESTRICT, db_column='promo_id', null=False, blank=False)
+    product_id = models.ForeignKey('Product', on_delete=models.RESTRICT, db_column='product_id', null=False, blank=False)
+
+
+
+class CartProduct(models.Model):
+
+    class Meta:
+        db_table = 'cart_products'
+        ordering = ['id']
+
+    product_id = models.ForeignKey('Product', on_delete=models.RESTRICT, db_column='product_id', null=False, blank=False)
+    quantity = models.SmallIntegerField(db_column='quantity', null=False, blank=False)
+    cart_id = models.ForeignKey('Cart', on_delete=models.RESTRICT, db_column='cart_id', null=False, blank=False)
+
+
+
+class Cart(models.Model):
+
+    class Meta:
+        db_table = 'carts'
+        ordering = ['id']
+
+    user_id = models.ForeignKey(User, on_delete=models.RESTRICT, db_column='user_id', null=False, blank=False)
+    date = models.BigIntegerField(db_column='date', null=False, blank=False)
+    price = models.IntegerField(db_column='price', null=False, blank=False)
+    retailer_id = models.ForeignKey('Retailer', on_delete=models.RESTRICT, db_column='retailer_id', null=False, blank=False)
+    cart_status = models.BooleanField( db_column='cart_status', null=False, blank=False, default=True)
--- scrap_try.py
+++ scrap_try.py
@@ -1,9 +1,17 @@
 from bs4 import BeautifulSoup
 import pandas as pd
 import requests
-import urllib.request
+# import urllib.request
+# import gzip
+# import io
+
+# import urllib.request as urllib2
+import json
 import gzip
 import io
+
+import urllib.request
+
 
 # http://prices.shufersal.co.il/FileObject/UpdateCategory?catID=0&storeId=413
 
@@ -26,23 +34,45 @@ if __name__ == '__main__':
     # print(a.find('a')['href'])
 
 
-    outFile = io.StringIO()
+    # outFile = io.StringIO()
 
-    # Set the URL for the gzipped file
-    url = 'http://pricesprodpublic.blob.core.windows.net/pricefull/PriceFull7290027600007-413-202303300340.gz?sv=2014-02-14&sr=b&sig=cydmUqV%2BjfGzgw9eI%2FEl5O562EMZbyOM77cT8u87Z7I%3D&se=2023-03-30T16%3A19%3A05Z&sp=r'
-    # Send a request to the URL and get the gzipped data
-    response = urllib.request.urlopen(url)
+    # # Set the URL for the gzipped file
+    # url = 'http://pricesprodpublic.blob.core.windows.net/pricefull/PriceFull7290027600007-413-202304010340.gz?sv=2014-02-14&sr=b&sig=zJ5Mr3ef18UjO0a%2F%2Bh0rzLWWMZUoMNDLN2TzWOEUQSk%3D&se=2023-04-01T19%3A43%3A38Z&sp=r'
+    # # Send a request to the URL and get the gzipped data
+    # response = urllib.request.urlopen(url)
+    #
+    # # Read the gzipped data into memory
+    # compressed_file = io.BytesIO(response.read())
+    # print(compressed_file)
+    #
+    # # Decompress the gzipped data
+    # decompressed_file = gzip.GzipFile()
+    # print(type(decompressed_file))
+    #
+    # with gzip.open(decompressed_file, mode='rb') as f:
+    #     file_content = f.read()
+    #     print(file_content)
+    #
+    # # Read the XML data using pandas
+    # # xml_data = pd.read_xml(decompressed_file)
+    #
+    # # Print the XML data
+    # # print(xml_data)
 
-    # Read the gzipped data into memory
-    compressed_file = io.BytesIO(response.read())
-    print(compressed_file.read())
+    # url = 'http://pricesprodpublic.blob.core.windows.net/pricefull/PriceFull7290027600007-413-202304010340.gz?sv=2014-02-14&sr=b&sig=zJ5Mr3ef18UjO0a%2F%2Bh0rzLWWMZUoMNDLN2TzWOEUQSk%3D&se=2023-04-01T19%3A43%3A38Z&sp=r'
+    # page = urllib2.urlopen(url)
+    # gzip_filehandle = gzip.GzipFile(fileobj=io.BytesIO(page.read()))
+    # print(gzip_filehandle)
+    # json_data = json.loads(gzip_filehandle.read())
+    # print(json_data)
 
-    # Decompress the gzipped data
-    decompressed_file = gzip.GzipFile(fileobj=compressed_file)
-    print(decompressed_file)
+    response = urllib.request.urlopen('http://pricesprodpublic.blob.core.windows.net/pricefull/PriceFull7290027600007-413-202304010340.gz?sv=2014-02-14&sr=b&sig=I0u4mqYQtGoQFf%2FHDNFq0tI5WTPM4nbb1od0DPXqzYk%3D&se=2023-04-01T20%3A21%3A51Z&sp=r')
+    with gzip.GzipFile(fileobj=response) as f:
+        # df = pd.read_xml(f.read(), compression='gzip')
+        xml_parser = BeautifulSoup(open(f), 'xml')
+    # # df = pd.read_xml(f ,  parser='lxml', compression='gzip')
+    # pd.set_option('display.max_columns', None)  # show all columns
+    # pd.set_option('display.max_rows', None)  # show all rows
+    # print(df)
 
-    # Read the XML data using pandas
-    xml_data = pd.read_xml(decompressed_file)n
 
-    # Print the XML data
-    print(xml_data)
