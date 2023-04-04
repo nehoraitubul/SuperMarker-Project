ada8fdfa8bcae5471ca8448b32654ff79eedd1fe
nehoraitubul <nehorai0258@gmail.com>
2023-04-04 11:00:33 +0300

    commit

--- scrap_try.py
+++ scrap_try.py
@@ -1,16 +1,10 @@
 from bs4 import BeautifulSoup
 import pandas as pd
 import requests
-# import urllib.request
-# import gzip
-# import io
-
-# import urllib.request as urllib2
-import json
+import xml.etree.ElementTree as ET
 import gzip
-import io
-
 import urllib.request
+# from onlineStoreApp.models import Product, Retailer, Price
 
 
 # http://prices.shufersal.co.il/FileObject/UpdateCategory?catID=0&storeId=413
@@ -19,60 +13,71 @@ import urllib.request
 
 if __name__ == '__main__':
 
-    # response = requests.get('http://prices.shufersal.co.il/FileObject/UpdateCategory?catID=0&storeId=413')
-    #
-    # soup = BeautifulSoup(response.text, 'html.parser')
-    #
-    # price_full_url = soup.find("td", string='pricefull')
-    # a = price_full_url.parent
-    # print('price full data link:')
-    # print(a.find('a')['href'])
-    #
-    # promo_full_url = soup.find("td", string='promofull')
-    # a = promo_full_url.parent
-    # print('promos full data link:')
-    # print(a.find('a')['href'])
+    def units(unit):
+        if unit == 'יחידה':
+            return 'units'
+        if unit == 'גרמים':
+            return 'g'
+        if unit == 'מיליליטרים':
+            return 'ml'
+        if unit == 'קילוגרמים':
+            return 'kg'
+        if unit == 'ליטרים':
+            return 'l'
 
 
-    # outFile = io.StringIO()
+    response = requests.get('http://prices.shufersal.co.il/FileObject/UpdateCategory?catID=0&storeId=413')
 
-    # # Set the URL for the gzipped file
-    # url = 'http://pricesprodpublic.blob.core.windows.net/pricefull/PriceFull7290027600007-413-202304010340.gz?sv=2014-02-14&sr=b&sig=zJ5Mr3ef18UjO0a%2F%2Bh0rzLWWMZUoMNDLN2TzWOEUQSk%3D&se=2023-04-01T19%3A43%3A38Z&sp=r'
-    # # Send a request to the URL and get the gzipped data
-    # response = urllib.request.urlopen(url)
-    #
-    # # Read the gzipped data into memory
-    # compressed_file = io.BytesIO(response.read())
-    # print(compressed_file)
-    #
-    # # Decompress the gzipped data
-    # decompressed_file = gzip.GzipFile()
-    # print(type(decompressed_file))
-    #
-    # with gzip.open(decompressed_file, mode='rb') as f:
-    #     file_content = f.read()
-    #     print(file_content)
-    #
-    # # Read the XML data using pandas
-    # # xml_data = pd.read_xml(decompressed_file)
-    #
-    # # Print the XML data
-    # # print(xml_data)
+    soup = BeautifulSoup(response.text, 'html.parser')
 
-    # url = 'http://pricesprodpublic.blob.core.windows.net/pricefull/PriceFull7290027600007-413-202304010340.gz?sv=2014-02-14&sr=b&sig=zJ5Mr3ef18UjO0a%2F%2Bh0rzLWWMZUoMNDLN2TzWOEUQSk%3D&se=2023-04-01T19%3A43%3A38Z&sp=r'
-    # page = urllib2.urlopen(url)
-    # gzip_filehandle = gzip.GzipFile(fileobj=io.BytesIO(page.read()))
-    # print(gzip_filehandle)
-    # json_data = json.loads(gzip_filehandle.read())
-    # print(json_data)
+    price_full_url = soup.find("td", string='pricefull')
+    a = price_full_url.parent
+    print('price full data link:')
+    price_url = a.find('a')['href']
+    print(price_url)
 
-    response = urllib.request.urlopen('http://pricesprodpublic.blob.core.windows.net/pricefull/PriceFull7290027600007-413-202304010340.gz?sv=2014-02-14&sr=b&sig=I0u4mqYQtGoQFf%2FHDNFq0tI5WTPM4nbb1od0DPXqzYk%3D&se=2023-04-01T20%3A21%3A51Z&sp=r')
-    with gzip.GzipFile(fileobj=response) as f:
-        # df = pd.read_xml(f.read(), compression='gzip')
-        xml_parser = BeautifulSoup(open(f), 'xml')
-    # # df = pd.read_xml(f ,  parser='lxml', compression='gzip')
-    # pd.set_option('display.max_columns', None)  # show all columns
-    # pd.set_option('display.max_rows', None)  # show all rows
-    # print(df)
+    promo_full_url = soup.find("td", string='promofull')
+    b = promo_full_url.parent
+    print('promos full data link:')
+    promo_url = b.find('a')['href']
+    print(promo_url)
 
 
+
+    # request to DataFrame
+    # response = urllib.request.urlopen(price_url)
+    #
+    # with gzip.open(response, 'rb') as f:
+    #     xml_data = f.read()
+    #
+    # root = ET.fromstring(xml_data)
+    #
+    # items_list = []
+    #
+    # # Loop through all Item elements
+    # for item in root.iter('Item'):
+    #     # Create a dictionary for the current item
+    #     item_dict = {}
+    #     # Loop through all sub-elements of the current item
+    #     for child in item:
+    #         item_dict[child.tag] = child.text
+    #     # Append the dictionary to the list of items
+    #     items_list.append(item_dict)
+    #
+    # df = pd.DataFrame(items_list)
+    #
+    # # Convert data types
+    # df['Quantity'] = pd.to_numeric(df['Quantity'])
+    # df['QtyInPackage'] = pd.to_numeric(df['QtyInPackage'])
+    # df['ItemPrice'] = pd.to_numeric(df['ItemPrice'])
+    # df['UnitOfMeasurePrice'] = pd.to_numeric(df['UnitOfMeasurePrice'])
+    #
+    # retailer = Retailer.objects.get(name='Shufersal')
+    #
+    # for index, row in df.iterrows():
+    #     product = Product.objects.get(catalog_number = row['ItemCode'])
+    #     product.name = row['ItemName']
+    #     product.quantity = row['Quantity']
+    #     product.product_status = True if row['ItemStatus'] == 1 else False
+    #     product.discount_status = True if row['AllowDiscount'] == 1 else False
+    #     product.unit_of_measure_price = row['UnitOfMeasurePrice']
